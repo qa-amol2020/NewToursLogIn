a2f4a37aab6788bcd2d3a1252ab751682be3d152
qa-amol2020 <qa.amolbegineer@gmail.com>
2020-01-13 11:30:08 +0530

    first commit

---
+++ BaseTest.java
@@ -0,0 +1,33 @@
+package com.DataDriven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	
+	public WebDriver driver = null;
+	public String url="http://newtours.demoaut.com";
+		
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "./driverFiles/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		
+		driver.manage().window().maximize();
+	//	driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	}
+
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
